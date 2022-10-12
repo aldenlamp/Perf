2135f8184c9eeb0512658ab6fa9b73f4ea1cf3e7
aldenlamp <aldenblamp@gmail.com>
2022-10-12 00:47:21 -0400

    Set Up Project

---
+++ perf_tracker/main.py
@@ -0,0 +1,3 @@
+def main() -> int:
+    print("This is a test")
+    return 0
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.0"
+PACKAGE_NAME = "Performance Tracker"
+REQUIREMENTS = ["yapf>=0.32.0", "matplotlib>=3.5.3"]
+
+setup(name=PACKAGE_NAME,
+      version=VERSION,
+      author="Alden Lamp",
+      author_email="aldenblamp@gmail.com",
+      description=("This is a simple tool to help me track the runtime and "
+                   "success stats of my C++ project"),
+      long_description=open("README.md", "r", encoding="utf-8").read(),
+      install_requires=REQUIREMENTS,
+      packages=find_packages(where="src", include=["perf_tracker"]),
+      entry_points={'console_scripts': ['perf = perf_tracker.main:main']})
