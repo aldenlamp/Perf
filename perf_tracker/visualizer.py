a35fa4df63fa836d350c079c1977143d4662e4ee
aldenlamp <aldenblamp@gmail.com>
2022-11-16 04:38:47 -0500

    Create generic pie plot function

---
+++ perf_tracker/visualizer.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+
+
+class Visualizer():
+
+    @staticmethod
+    def create_pie_chart(values: list[float],
+                         labels: list[str],
+                         out_dir: Path,
+                         plot_name: str = "pie_plot"):
+
+        _, ax = plt.subplots(subplot_kw=dict(aspect="equal"))
+        wedges, _ = ax.pie(values, startangle=0, labels=None)  # type: ignore
+        box = ax.get_position()
+
+        ax.set_position(
+            [box.x0 - (.25 * box.width), box.y0, box.width, box.height])
+
+        ax.legend(wedges,
+                  labels,
+                  loc="center left",
+                  bbox_to_anchor=(0.9, 0, 0.5, 1))
+
+        plt.savefig(out_dir / f"{plot_name}.png")
